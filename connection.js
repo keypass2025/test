1bc3c21a38823d1d4a9b1666c310e8c5a0cef72c
keypass2025 <keypass2025@gmail.com>
2025-01-15 18:14:02 +0530

    index

---
+++ connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connection = mongoose.connect("mongodb+srv://must:must@cluster0.9ubch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+
+
+
+// mongodb+srv://must:must@2000@cluster0.qmyvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// mongodb+srv://sm2025:*123456789*@dlxprint.sikdfoy.mongodb.net/?retryWrites=true&w=majority&appName=dlxprint
+
+// module.exports = connection;
+// 
+// mongodb+srv://dlxprinting02:zWRqbgMNVjnOa2G0@dlxprint.sikdfoy.mongodb.net/?retryWrites=true&w=majority&appName=dlxpri
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express()
+app.use(express.json());
+
+const connection = require("./connection");
+
+const UserModel = require('./userModel');
+
+
+// const userRouter = express.Router();
+
+
+app.post('/login', async (req, res) => {
+    const { password } = req.body;
+    try {
+        const newPass = password;
+        const user = await UserModel.create({ ...req.body, password: newPass });
+        res.status(200).send({ 'msg': 'User registered successfully', user })
+    } catch (error) {
+        res.status(400).send({ 'msg': error.message });
+    }
+})
+
+// app.use(userRouter);
+
+// app.use(cors());
+// app.use('/donation', donationRouter);
+// app.use('/admin',adminRouter)
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send({'msg' : error.message});
+    }
+    
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Home Page");
+  });
+  
+
+app.listen(8080, async () => {
+  try {
+    // const mongoose = require('mongoose');
+    // require('dotenv').config();
+
+    // mongoose.connect('mongodb+srv://sm2025:*123456789*@cluster0.v6880.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0');
+    await connection;
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+// mongodb+srv://nyvive:o5yPRr6p0cP78Tw5@cluster0.v6880.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// mongodb+srv://nyvive:o5yPRr6p0cP78Tw5@cluster0.v6880.mongodb.net/?retryWrites=true&w=majority&appName=users
+
+
+// mongodb+srv://<sm2025>:<*123456789*>@cluster0.v6880.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
