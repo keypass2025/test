932cbb3d8c53f22f6ac1f58f3a7510c07fd3dafe
keypass2025 <keypass2025@gmail.com>
2025-01-16 11:10:02 +0530

    cors update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express()
 app.use(express.json());
 
+var cors = require('cors')
+app.use(cors())
+
+
 const connection = require("./connection");
 
 const UserModel = require('./userModel');
